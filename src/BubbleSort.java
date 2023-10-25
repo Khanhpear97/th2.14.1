66fa3a4c18e9061035197ba91c5560e61fd5fb97
Khanhpear97 <khanhpear97@gmail.com>
2023-10-25 15:16:04 +0700

    first commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static void bubbleSort(int[] list) {
+        boolean needNextPart = true;
+
+        for (int i = 1; i < list.length && needNextPart; i++) {
+            needNextPart = false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+
+                    needNextPart = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i] + " ");
+        }
+    }
+}
